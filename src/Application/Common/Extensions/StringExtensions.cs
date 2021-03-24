5225746ece7d51970444e4d6928550ba54cd00de
Junyoung Bang <jun000ang@gmail.com>
2021-03-24 11:05:31 +0700

    feat: string extensions

--- src/Application/Common/Extensions/StringExtensions.cs
+++ src/Application/Common/Extensions/StringExtensions.cs
@@ -27,6 +27,16 @@ namespace NetCoreCleanArchitecture.Application.Common.Extensions
 {
     public static class StringExtensions
     {
+        public static string Serialize<T>(this T value) where T : class
+        {
+            if (value is null) return "{}";
+
+            return JsonSerializer.Serialize<T>(value, new JsonSerializerOptions(JsonSerializerDefaults.Web)
+            {
+                NumberHandling = JsonNumberHandling.AllowReadingFromString,
+            });
+        }
+
         public static T Deserialize<T>(this string value) where T : class
         {
             if (string.IsNullOrWhiteSpace(value)) return default;
@@ -66,6 +76,216 @@ namespace NetCoreCleanArchitecture.Application.Common.Extensions
         public static string RemoveExtraSpaces(this string value)
         {
             return string.IsNullOrWhiteSpace(value) ? string.Empty : Regex.Replace(value, @"\s+", " ");
+        }
+
+        public static string IntergerCharacters(this string value)
+        {
+            if (value is null) return "0";
+
+            var cnt = value.Count();
+
+            var isStarted = false;
+
+            var sb = new StringBuilder(cnt);
+
+            foreach (var c in value)
+            {
+                if (char.IsControl(c)) continue;
+                switch (c)
+                {
+                    case ' ':
+                        break;
+                    case '.':
+                        goto stop;
+                    case '-':
+                        if (!isStarted) sb.Append(c);
+                        else goto stop;
+                        break;
+                    case '0':
+                        {
+                            sb.Append(c);
+                            isStarted = true;
+                        }
+                        break;
+                    case '1':
+                        {
+                            sb.Append(c);
+                            isStarted = true;
+                        }
+                        break;
+                    case '2':
+                        {
+                            sb.Append(c);
+                            isStarted = true;
+                        }
+                        break;
+                    case '3':
+                        {
+                            sb.Append(c);
+                            isStarted = true;
+                        }
+                        break;
+                    case '4':
+                        {
+                            sb.Append(c);
+                            isStarted = true;
+                        }
+                        break;
+                    case '5':
+                        {
+                            sb.Append(c);
+                            isStarted = true;
+                        }
+                        break;
+                    case '6':
+                        {
+                            sb.Append(c);
+                            isStarted = true;
+                        }
+                        break;
+                    case '7':
+                        {
+                            sb.Append(c);
+                            isStarted = true;
+                        }
+                        break;
+                    case '8':
+                        {
+                            sb.Append(c);
+                            isStarted = true;
+                        }
+                        break;
+                    case '9':
+                        {
+                            sb.Append(c);
+                            isStarted = true;
+                        }
+                        break;
+                    default:
+                        {
+                            if (isStarted) goto stop;
+                        }
+                        break;
+                }
+            }
+        stop:
+            return sb.ToString();
+        }
+
+        public static string FloatCharacters(this string value)
+        {
+            if (value is null) return "0";
+
+            var cnt = value.Count();
+
+            var hasDot = false;
+
+            var isStarted = false;
+
+            var sb = new StringBuilder(cnt);
+
+            foreach (var c in value)
+            {
+                if (char.IsControl(c)) continue;
+                switch (c)
+                {
+                    case ' ':
+                        break;
+                    case '.':
+                        if (!hasDot)
+                        {
+                            sb.Append(c);
+                            hasDot = true;
+                        }
+                        else goto stop;
+                        break;
+                    case '-':
+                        if (!isStarted) sb.Append(c);
+                        else goto stop;
+                        break;
+                    case '0':
+                        {
+                            sb.Append(c);
+                            isStarted = true;
+                        }
+                        break;
+                    case '1':
+                        {
+                            sb.Append(c);
+                            isStarted = true;
+                        }
+                        break;
+                    case '2':
+                        {
+                            sb.Append(c);
+                            isStarted = true;
+                        }
+                        break;
+                    case '3':
+                        {
+                            sb.Append(c);
+                            isStarted = true;
+                        }
+                        break;
+                    case '4':
+                        {
+                            sb.Append(c);
+                            isStarted = true;
+                        }
+                        break;
+                    case '5':
+                        {
+                            sb.Append(c);
+                            isStarted = true;
+                        }
+                        break;
+                    case '6':
+                        {
+                            sb.Append(c);
+                            isStarted = true;
+                        }
+                        break;
+                    case '7':
+                        {
+                            sb.Append(c);
+                            isStarted = true;
+                        }
+                        break;
+                    case '8':
+                        {
+                            sb.Append(c);
+                            isStarted = true;
+                        }
+                        break;
+                    case '9':
+                        {
+                            sb.Append(c);
+                            isStarted = true;
+                        }
+                        break;
+                    default:
+                        {
+                            if (isStarted) goto stop;
+                        }
+                        break;
+                }
+            }
+        stop:
+            return sb.ToString();
+        }
+
+        public static int ToInt32(this string value)
+        {
+            var val = IntergerCharacters(value);
+
+            return string.IsNullOrWhiteSpace(val) ? 0 : Convert.ToInt32(val);
+        }
+
+        public static double ToDouble(this string value)
+        {
+            var val = FloatCharacters(value);
+
+            return string.IsNullOrWhiteSpace(val) ? 0 : Convert.ToDouble(val);
         }
     }
 }
