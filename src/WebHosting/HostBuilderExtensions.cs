2b2a5aad0eacc4460295538a7740ef2bf40625a9
Junyoung Bang <jun000ang@gmail.com>
2021-03-27 10:11:20 +0700

    fixL logging level

--- src/WebHosting/HostBuilderExtensions.cs
+++ src/WebHosting/HostBuilderExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Serilog;
+using Serilog.Core;
 using Serilog.Enrichers.Span;
 using System;
 
@@ -11,6 +12,7 @@ namespace NetCoreCleanArchitecture.WebHosting
         private const string APP_NAME_SECTION = "AppName";
         private const string APP_ID_SECTION = "AppId";
         private const string SEQ_URL_SECTION = "SeqServerUrl";
+        private const string SEQ_APIKEY_SECTION = "SeqApiKey";
 
         public static IHostBuilder UseSerilog(this IHostBuilder builder, string[] args)
         {
@@ -24,17 +26,21 @@ namespace NetCoreCleanArchitecture.WebHosting
             var appName = configuration.GetValue<string>(APP_NAME_SECTION);
             var appId = configuration.GetValue<string>(APP_ID_SECTION);
             var serverUrl = configuration.GetValue<string>(SEQ_URL_SECTION);
+            var apiKey = configuration.GetValue<string>(SEQ_APIKEY_SECTION);
+
+            var levelSwitch = new LoggingLevelSwitch();
 
             Log.Logger = new LoggerConfiguration().ReadFrom
                 .Configuration(configuration)
+                .MinimumLevel.ControlledBy(levelSwitch)
                 .Enrich.FromLogContext()
                 .Enrich.WithMachineName()
                 .Enrich.WithThreadId()
                 .Enrich.WithClientIp()
                 .Enrich.WithSpan()
-                .Enrich.WithProperty("ApplicationName", string.IsNullOrWhiteSpace(appId) ? string.IsNullOrWhiteSpace(appName) ? "unknown" : appName : appId )
+                .Enrich.WithProperty("ApplicationName", string.IsNullOrWhiteSpace(appId) ? string.IsNullOrWhiteSpace(appName) ? "unknown" : appName : appId)
                 .WriteTo.Console()
-                .WriteTo.Seq(string.IsNullOrWhiteSpace(serverUrl) ? "http://seq" : serverUrl)
+                .WriteTo.Seq(string.IsNullOrWhiteSpace(serverUrl) ? "http://seq" : serverUrl, apiKey: apiKey, controlLevelSwitch: levelSwitch)
                 .CreateLogger();
 
             SerilogHostBuilderExtensions.UseSerilog(builder);
